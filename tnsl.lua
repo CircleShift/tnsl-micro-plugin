d53937615f033357b1d0c6087afe776542b0a0d0
Kyle Gunger <kgunger12@gmail.com>
2022-12-07 14:00:34 -0500

    Add syntax and autoclose

---
+++ tnsl.lua
@@ -0,0 +1,29 @@
+VERSION = "0.1.0"
+
+local micro = import("micro")
+local config = import("micro/config")
+local fmt = import("fmt")
+local uutil = import("micro/util")
+
+
+function init()
+	config.AddRuntimeFile("tnsl", config.RTSyntax, "syntax.yaml")
+end
+
+function onRune(bp, r)
+	if bp.Buf:FileType() == "tnsl" then
+
+		local curLine = bp.Buf:Line(bp.Cursor.Y)
+		if bp.Cursor.X > 1 and (uutil.RuneAt(curLine, bp.Cursor.X - 2) == '/') then
+			
+			local close = uutil.RuneAt(curLine, bp.Cursor.X - 1)
+			if close == ';' or close == ':' or close == '#' then
+				
+				bp.Buf:Insert(-bp.Cursor.Loc, close)
+				bp.Buf:Insert(-bp.Cursor.Loc, '/')
+				bp:CursorLeft()
+				bp:CursorLeft()
+			end
+		end
+	end
+end
